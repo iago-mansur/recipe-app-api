738b754d80d8f5a4d3b00358aa1f7b862f532707
iago-mansur <iagoma@gmail.com>
2023-06-27 14:51:19 -0300

    Build recipe API.

--- README.md
+++ README.md
@@ -82,3 +82,16 @@ docker-compose run --rm app sh -c "django-admin startapp user"
 docker-compose run --rm app sh -c "python manage.py test"
 
 docker-compose up
+
+Email: user2@example.com
+Password: Awesome!23
+
+docker-compose run --rm app sh -c "python manage.py test"
+
+docker-compose run --rm app sh -c "python manage.py makemigrations"
+
+docker-compose run --rm app sh -c "django-admin startapp recipe"
+
+docker-compose run --rm app sh -c "python manage.py test"
+
+docker-compose up
--- app/user/serializers.py
+++ app/user/serializers.py
@@ -4,7 +4,7 @@ Serializers for the user API View.
 from django.contrib.auth import (
     get_user_model,
     authenticate,
-    )
+)
 from django.utils.translation import gettext as _
 
 from rest_framework import serializers
@@ -21,7 +21,7 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
-    
+
     def update(self, instance, validated_data):
         """Update and return user."""
         password = validated_data.pop('password', None)
